1c333236e519ce87e7fa54a096a23d37d60d25bf
SobhanSaeedi7 <Sobi.S.K2007@gmail.com>
2024-01-15 14:12:59 +0330

    generate_dataset.py updated

--- 1.Face_Recognition/generate_dataset.py
+++ 1.Face_Recognition/generate_dataset.py
@@ -4,19 +4,20 @@ from tqdm import tqdm
 from deepface import DeepFace
 
 def generate_dataset(path='/content/drive/MyDrive/PersianFace'):
-    dataset = []
+    data_frame = pd.DataFrame()
+    Celebs = []
 
     for folder in tqdm(os.listdir(path)):
         for img in os.listdir(f'{path}/{folder}'):
             try:
-                embedding_objs = DeepFace.represent(img_path = f'{path}/{folder}/{img}', model_name='ArcFace')
+                embedding_objs = DeepFace.represent(img_path = f'{path}/{folder}/{img}', model_name='ArcFace', enforce_detection=False)
             except:
                 pass
-            dict_img = embedding_objs[0]
-            dict_img['label'] = folder
-            dataset.append(dict_img) 
+            df = pd.DataFrame([embedding_objs[0]['embedding']])
+            df['Celeb'] = folder
+            Celebs.append(folder) if folder not in Celebs else Celebs
+            df['Celeb_Label'] = Celebs.index(folder)
+            data_frame = data_frame.append(df, ignore_index = True)
 
-    df = pd.DataFrame(dataset)
-    df.to_csv('dataset/dataset.csv')
-
-
+    data_frame.to_csv('dataset/dataset.csv')
+    data_frame.head()
